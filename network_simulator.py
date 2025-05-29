a3d6571de2b57d7719cd7fc23089caa2e7477363
Inigo Joseph <inigopopuvinodhan_bu2@mail.sjctni.edu>
2025-05-29 08:39:29 +0530

    Update network_simulator.py

--- network_simulator.py
+++ network_simulator.py
@@ -73,7 +73,7 @@ st.markdown("""
         transition: all 0.3s ease;
     }
     .footer {
-        position: sticky;
+        position: inherit;
         left: 0;
         bottom: 0;
         width: 100%;
